6f9f0266b75f09898b259945361d241ae630d41f
arten <felipe.arten@gmail.com>
2024-08-23 19:17:11 -0300

    feat: resolução desafio conversor kelvin - celsius

---
+++ src/ConversorTemperatura/conversor.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ebulicaoK := 373
+
+	ebulicaoC := ebulicaoK - 273
+
+	fmt.Printf("A temperatura de ebulição da água em Kelvin é de: %d K\nA temperatura de ebulição da água em Celsius é de: %d °C", ebulicaoK, ebulicaoC)
+}
